a760a749986e3f1a2cb14ceb969225d34eae744a
tohid rajabali <tohidrajabali1380@gmail.com>
2025-04-15 18:28:15 +0330

    Add project files.

---
+++ DongiApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DongiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ DongiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DongiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
